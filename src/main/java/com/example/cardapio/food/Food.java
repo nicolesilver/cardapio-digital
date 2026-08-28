948cc409e1dc93542dcaa967d727b97eb57aa86a
nicolesilver <nicolesilveira1607@gmail.com>
2026-08-27 21:45:48 -0300

    first commit - Projeto estruturado

---
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,26 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "foods")
+@Entity(name ="foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (of ="id")
+
+public class
+Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // identificar que ela representa o id e é gerada automaticamente
+    private String title;
+    private String image; // url
+    private Integer price;
+
+    public Food(FoodRequestDTO data) {
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
+}
